4350a130d12425ffd5ec4f57249e9d52848d33f9
Stephen Allinson <stephen.allinson@gmail.com>
2025-05-06 14:28:33 -0500

    Staticcheck and expect failure

--- main.go
+++ main.go
@@ -11,10 +11,9 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
-
-	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type apiConfig struct {
@@ -23,6 +22,11 @@ type apiConfig struct {
 
 //go:embed static/*
 var staticFiles embed.FS
+
+func unused() {
+	// this function does nothing
+	// and is called nowhere
+}
 
 func main() {
 	err := godotenv.Load(".env")
